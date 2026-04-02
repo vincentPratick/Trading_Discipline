a2130167e03b3899c014f9caf49c367c66f99f1b
vincentPratick <vincentteh2022@gmail.com>
2026-04-02 23:16:05 +0800

    Add service worker with install, activate, and fetch events

---
+++ public/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", () => {
+  // 先留空，最简版就够了
+});
